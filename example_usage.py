fd9219414582804d7d52128f156baa0b4e876074
harshbhatt <harshbhatt.me@gmail.com>
2014-11-03 18:44:01 +0800

    Updated commands for example purposes.

--- example_usage.py
+++ example_usage.py
@@ -1,5 +1,4 @@
 from eBot import *
-from time import time
 
 #Create new instance of eBot - connects to first eBot the computer is connected to
 myEBot = eBot()
@@ -11,27 +10,23 @@ myEBot.halt()
 sleep(1)
 obs = myEBot.obstacle()
 
-#myEBot.led(1)
-#sleep(1)
-#myEBot.led(0)
-#sleep(0.5)
-#myEBot.led(1)
-#myEBot.wheels(.2,.2)
-#sleep(2)
-#myEBot.close()
-#sleep(1)
-#myEBot.open()
-#sleep(1)
-#myEBot.wheels(.2,.2)
-#sleep(2)
-#myEBot.halt()
-#sleep(1)
-while True:
-    myvalue = myEBot.robot_uS()
+myEBot.led(1)
+sleep(1)
+myEBot.led(0)
+sleep(1)
 
-    #if myvalue[2] < 0.300:
-    #   myEBot.halt()
-    #else:
-    #   myEBot.wheels(1,1)
-    print myvalue
-myEBot.disconnect()
+myEBot.wheels(.2,.2)
+sleep(2)
+myEBot.halt()
+sleep(1)
+
+for i in range(1, 1000, 1):
+    sonars = myEBot.robot_uS()
+    if sonars[2] < 0.300:
+        myEBot.halt()
+    else:
+        myEBot.wheels(1, 1)
+    print sonars
+
+myEBot.halt()
+myEBot.close()
