47e749ee3792c6d2396ca0aa80e42bc4fbf1e7b8
abhishek <abhishek@sutd.edu.sg>
2015-09-28 22:34:03 +0800

    striaght line test

--- eBot/straight_line.py
+++ eBot/straight_line.py
@@ -1,21 +1,18 @@
 from eBot import *
 import time
-#test
 #Create new instance of eBot - connects to first eBot the computer is connected to
 myEBot = eBot()
-myEBot.wheels(2, 2)
 myEBot.connect()
-accel = [0, 0, 0, 0]
+accel = [0, 0, 0, 0, 0, 0]
 myEBot.halt()
+while accel[1] < 100:
+    accel = myEBot.robot_uS()
+    sleep(0.05)
+    print accel
 
-myEBot.wheels(0.3, 0.3)
 now_time = time.time()
-#while accel[1] < 1000:
-#    accel = myEBot.position()
-#    print acce
-while time.time() - now_time < 10:
+while time.time() - now_time < 5:
     accel = myEBot.position()
     print accel
-myEBot.wheels(0, 0)
 myEBot.halt()
 myEBot.close()
